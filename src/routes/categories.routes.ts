8769c883fd03ad194e25040e5bf8e83a221dc35f
Eduardo <eduardolimacruz2@gmail.com>
2022-10-19 23:01:43 -0300

    inicialização do projeto

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,16 @@
+import { Router } from "express";
+
+import { createCategoryController } from "../modules/cars/useCases/createCategory";
+import { listCategoriesController } from "../modules/cars/useCases/listCategories";
+
+const categoriesRoutes = Router();
+
+categoriesRoutes.post("/", (request, response) => {
+  return createCategoryController.handle(request, response);
+});
+
+categoriesRoutes.get("/", (_request, response) => {
+  return listCategoriesController.handle(_request, response);
+});
+
+export { categoriesRoutes };
